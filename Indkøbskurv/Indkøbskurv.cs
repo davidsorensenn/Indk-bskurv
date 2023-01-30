c33d47dded6c904a55f8b365225ffab26514c3cd
David Worm Sørensen <davi6583@edu.sde.dk>
2023-01-30 13:24:27 +0100

    Add project files.

---
+++ Indkøbskurv/Indkøbskurv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Indkøbskurv
+{
+    internal class Indkøbskurv
+    {
+        public List<Produkter> produkter  { get; set; }
+
+
+        public static string Order(List<Produkter> items)
+        {
+            //create new lists of items
+            List<Produkter> itemsOrdered = new List<Produkter>();
+            //loop through items and add to items ordered
+            foreach (var item in items)
+            {
+                //check if item is already in list
+                var itemInList = itemsOrdered.FirstOrDefault(x => x.id == item.id);
+                if (itemInList != null)
+                {
+                    //if item is in list, increase quantity
+                    itemInList.quantity += item.quantity;
+                }
+                else
+                {
+                    //if item is not in list, add to list
+                    itemsOrdered.Add(item);
+                }
+                
+            }
+            return JsonSerializer.Serialize<List<Produkter>>(itemsOrdered);
+
+        }
+    }
+}
---
+++ Indkøbskurv/Produkter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace Indkøbskurv
+{
+    internal class Produkter
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+
+       
+         
+        
+
+
+    }
+}
