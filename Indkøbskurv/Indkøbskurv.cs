301ee311d3090374cecd12ba111b323671c5f4e1
Kenni Kollemorten <kenn7575@gmail.com>
2023-01-31 19:38:30 +0100

    v1.4

--- Indkøbskurv/Indkøbskurv.cs
+++ Indkøbskurv/Indkøbskurv.cs
@@ -12,48 +12,14 @@ namespace Indkøbskurv
         List<Produkt> produkter { get; set; }
 
 
-        internal string Order(List<Produkt> items)
-        {
-            //create new lists of items
-            List<Produkt> itemsOrdered = new List<Produkt>();
-            //loop through items and add to items ordered
-            foreach (var item in items)
-            {
-                //check if item is already in list
-                var itemInList = itemsOrdered.FirstOrDefault(x => x.Id == item.Id);
-                if (itemInList != null)
-                {
-                    //if item is in list, increase quantity
-                    itemInList.Quantity += item.Quantity;
-                }
-                else
-                {
-                    //if item is not in list, add to list
-                    itemsOrdered.Add(item);
-                }
-                
-                
-            }
-            
-            return JsonSerializer.Serialize<List<Produkt>>(itemsOrdered);
-
-
-        }
+        
         internal void AddtoShoppinglist( Produkt produkt)
         {
-            
-
-            
             produkter.Add(produkt);
-
-
         }
         internal void RemoveFromShoppinglist(int index)
         {
-          
                 produkter.RemoveAt(index);
-
-            
         }
 
 
